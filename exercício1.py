559d398454656533b5d7fdb04828b4c65c227d7f
Kcn-Sec <kolnckk026@gmail.com>
2026-05-15 21:37:10 -0300

    Add files via upload

---
+++ exercício1.py
@@ -0,0 +1,20 @@
+A = [1, 0, 5, -2, -5, 7]
+# A[0] = 1
+# A[1] = 0
+# A[2] = 5
+# A[3] = -2
+# A[4] = -5
+# A[5] = 7
+
+soma = A[0] + A[1] + A[5]
+print(f'Soma de A[0] + A[1] + A[5] = {soma}')
+
+A[4] = 100
+
+print('\nValores do vetor A:')
+print(A[0])
+print(A[1])
+print(A[2])
+print(A[3])
+print(A[4])
+print(A[5])
